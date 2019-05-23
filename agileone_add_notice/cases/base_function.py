8e4c675c5f9de9e408f04330fc0636b649ce240f
aragale <yz>
2019-05-23 10:43:01 +0800

    将log改为print

--- agileone_add_notice/cases/base_function.py
+++ agileone_add_notice/cases/base_function.py
@@ -1,8 +1,9 @@
 import time
-import logging
+# import logging
 from selenium import webdriver
 
-log = logging.getLogger(__name__)
+# log = logging.getLogger(__name__)
+
 
 class BaseFunc:
     def __init__(self):
@@ -28,19 +29,19 @@ class BaseFunc:
     def check_msg(self):
         time.sleep(1)
         msg = self.driver.find_element_by_id("msg").text
-        log.info(msg)
+        print(msg)
         if "成功" in msg:
-            log.info("新增[标题为：%s，内容为：%s] 公告成功" % (self.headline, self.content))
+            print("新增[标题为：%s，内容为：%s] 公告成功" % (self.headline, self.content))
         else:
-            log.info("新增[标题为：%s，内容为：%s] 公告失败" % (self.headline, self.content))
+            print("新增[标题为：%s，内容为：%s] 公告失败" % (self.headline, self.content))
 
     def __del__(self):
-        self.driver.close()
-        # self.driver.quit()
+        self.driver.quit()
+    #     self.driver.close()
 
 
-if __name__ == '__main__':
-    b = BaseFunc()
-    b.login()
-    b.add_notice("this is a test", "this is a test of adding notice")
-    b.check_msg()
+# if __name__ == '__main__':
+#     b = BaseFunc()
+#     b.login()
+#     b.add_notice("this is a test", "this is a test of adding notice")
+#     b.check_msg()
